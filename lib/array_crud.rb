317f0f1a88b3b28d2a780c9acb53fe830b286bb8
Angela <malaika.mumba@gmail.com>
2019-05-06 15:42:28 -0400

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,45 @@
 def create_an_empty_array
-  
+  arr = Array.new
 end
 
 def create_an_array
-  
+arr = ["lion","hippo","rhino","elephant"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+array = ["lion","hippo","rhino","elephant"]
+element = "arrays!"
+array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["lion","hippo","rhino","elephant"]
+element = "wow"
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["wow","hippo","rhino","arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow","hippo","rhino","elephant"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["wow","hippo","am","elephant"]
+  index_number = 2
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+array = ["wow","hippo","rhino","elephant"]
+array.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+array = ["wow","hippo","rhino","arrays!"]
+array.last
 end
